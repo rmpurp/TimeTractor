4d168de757c111b7ff7eb55f45c2f3dc4aa8c173
Ryan Purpura <ryan@rpurp.com>
2020-04-28 14:17:21 -0700

    Initial Commit

---
+++ TimeTractor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTractor
+//
+//  Created by Ryan Purpura on 4/28/20.
+//  Copyright © 2020 Ryan Purpura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
